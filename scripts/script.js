a48ae846619a3b19cbc82ce4ef15ba4af63614f8
Matheus <matheuspedrosa307@gmail.com>
2024-06-24 14:39:16 -0300

    feat: recuperação de algumas informações na página de reserva

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ const inputs = document.querySelectorAll('.kits input[name="kit"]');
 
 inputs.forEach(input => {
     input.addEventListener('change', () => {
-        if(input.checked) {
+        if (input.checked) {
             const label = input.nextElementSibling;
             const titulo = label.querySelector('.kit-titulo').textContent;
             const descricao = label.querySelector('p').textContent;
@@ -19,3 +19,23 @@ inputs.forEach(input => {
         }
     })
 })
+
+const carroEscolhido = JSON.parse(localStorage.getItem('carroEscolhido'));
+
+const infosReserva = localStorage.getItem('data');
+
+if (infosReserva) {
+    const infos = JSON.parse(infosReserva);
+    document.querySelector('.qntd-dias').innerHTML = `<strong>Período do contrato:</strong> ${infos.diferenca} dia(s)`;
+    document.querySelector('.local-retirada').innerHTML = `<strong>Retirada:</strong> ${infos.local}`;
+    document.querySelector('.dia-retirada').innerHTML = `<strong>Data:</strong> ${infos.retirada}`;
+    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
+    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
+    document.querySelector('.dia-devolucao').innerHTML = `<strong>Data:</strong> ${infos.devolucao}`;
+}
+
+if (carroEscolhido) { 
+    document.querySelector('.nome-carro').innerHTML = `${carroEscolhido.modelo}`;
+    document.querySelector('.carro-imagem').src = `${carroEscolhido.imagem}`;
+    document.querySelector('.preco').innerHTML = `R$${carroEscolhido.preco}`;
+}
