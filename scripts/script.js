56138efb0615a016201fa5929e5d32707ccff6b1
Matheus <matheuspedrosa307@gmail.com>
2024-06-25 13:53:40 -0300

    ops: recuperação total das informações do carro escolhido

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,41 @@
+const carroEscolhido = JSON.parse(localStorage.getItem('carroEscolhido'));
+const infosReserva = localStorage.getItem('data');
+const infos = JSON.parse(infosReserva);
+
+function formatarDataParaExibicao(dataString) {
+    const date = new Date(dataString);
+    const dia = String(date.getDate() + 1).padStart(2, '0');
+    const mes = String(date.getMonth() + 1).padStart(2, '0');
+    const ano = date.getFullYear();
+    return `${dia}-${mes}-${ano}`;
+}
+
+if (infosReserva) {
+
+    const retiradaFormatada = formatarDataParaExibicao(infos.retirada);
+    const devolucaoFormatada = formatarDataParaExibicao(infos.devolucao);
+
+    document.querySelector('.qntd-dias').innerHTML = `<strong>Período do contrato:</strong> ${infos.intervalo} dia(s)`;
+    document.querySelector('.local-retirada').innerHTML = `<strong>Retirada:</strong> ${infos.local}`;
+    document.querySelector('.dia-retirada').innerHTML = `<strong>Data:</strong> ${retiradaFormatada}`;
+    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
+    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
+    document.querySelector('.dia-devolucao').innerHTML = `<strong>Data:</strong> ${devolucaoFormatada}`;
+}
+
+if (carroEscolhido) {
+    document.querySelector('.nome-carro').innerHTML = `${carroEscolhido.modelo}`;
+    document.querySelector('.carro-imagem').src = `${carroEscolhido.imagem}`;
+    document.querySelector('.preco').innerHTML = `R$${carroEscolhido.preco.replace('.', ',')}`;
+}
+
 const kitSelecionado = document.querySelector('.kit-selecionado');
 pacoteKit = kitSelecionado.querySelector('.pacote-kit');
 descricaoKit = kitSelecionado.querySelector('.descricao-kit');
 precoKit = kitSelecionado.querySelector('.preco');
+let precoFinal = document.querySelector('.preco-final');
+precoFinal.innerHTML = `R$${(carroEscolhido.preco * infos.intervalo).toFixed(2)}`.replace('.', ',');
+let valorBase = Number(`${(carroEscolhido.preco * infos.intervalo)}`);
 
 const inputs = document.querySelectorAll('.kits input[name="kit"]');
 
@@ -16,40 +50,8 @@ inputs.forEach(input => {
             pacoteKit.innerHTML = titulo;
             descricaoKit.innerHTML = descricao;
             precoKit.innerHTML = preco;
+            let conversaoDePreco = Number(preco.slice(2));
+            precoFinal.innerHTML = `R$${(conversaoDePreco + valorBase).toFixed(2)}`.replace('.', ',');
         }
     })
 })
-
-const carroEscolhido = JSON.parse(localStorage.getItem('carroEscolhido'));
-
-function formatarDataParaExibicao(dataString) {
-    const date = new Date(dataString);
-    const dia = String(date.getDate() + 1).padStart(2, '0');
-    const mes = String(date.getMonth() + 1).padStart(2, '0');
-    const ano = date.getFullYear();
-    return `${dia}-${mes}-${ano}`;
-}
-
-const infosReserva = localStorage.getItem('data');
-
-if (infosReserva) {
-    const infos = JSON.parse(infosReserva);
-
-    const retiradaFormatada = formatarDataParaExibicao(infos.retirada);
-    const devolucaoFormatada = formatarDataParaExibicao(infos.devolucao);
-
-    document.querySelector('.qntd-dias').innerHTML = `<strong>Período do contrato:</strong> ${infos.intervalo} dia(s)`;
-    document.querySelector('.local-retirada').innerHTML = `<strong>Retirada:</strong> ${infos.local}`;
-    document.querySelector('.dia-retirada').innerHTML = `<strong>Data:</strong> ${retiradaFormatada}`;
-    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
-    document.querySelector('.local-devolucao').innerHTML = `<strong>Devolução:</strong> ${infos.local}`;
-    document.querySelector('.dia-devolucao').innerHTML = `<strong>Data:</strong> ${devolucaoFormatada}`;
-}
-
-if (carroEscolhido) { 
-    document.querySelector('.nome-carro').innerHTML = `${carroEscolhido.modelo}`;
-    document.querySelector('.carro-imagem').src = `${carroEscolhido.imagem}`;
-    document.querySelector('.preco').innerHTML = `R$${carroEscolhido.preco}`;
-}
-
-
