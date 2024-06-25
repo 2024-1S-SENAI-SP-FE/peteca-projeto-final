73c0777958ef0ca3c48fee39b06a7f2558e56205
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-06-25 11:45:13 -0300

    feat: adicionando toques finais para a animação de flip

--- scripts/aluguel.js
+++ scripts/aluguel.js
@@ -91,7 +91,7 @@ const dadosCarros = [
     valorAluguel: "$450/month",
     detalhesLink: "#detalhesB",
   },
- 
+
   {
     nome: "Ranger P703",
     ano: "2023",
@@ -106,7 +106,6 @@ const dadosCarros = [
     valorAluguel: "$450/month",
     detalhesLink: "#detalhesB",
   },
-
 ];
 
 function criaContainerImg(imgSrc) {
@@ -115,7 +114,7 @@ function criaContainerImg(imgSrc) {
 
   const img = document.createElement("img");
   img.classList.add("img-carros");
-  img.src = imgSrc; 
+  img.src = imgSrc;
   containerImg.append(img);
 
   return containerImg;
@@ -127,17 +126,17 @@ function criaContainerNomeAno(nome, ano) {
 
   const spanNome = document.createElement("span");
   spanNome.classList.add("nome-carro");
-  spanNome.textContent = nome; 
+  spanNome.textContent = nome;
 
   const spanAno = document.createElement("span");
   spanAno.classList.add("ano-carro");
-  spanAno.textContent = ano; 
+  spanAno.textContent = ano;
 
   containerNomeAno.append(spanNome, spanAno);
   return containerNomeAno;
 }
 
-let a 
+let a;
 function criaContainerInfoCarro(valorAluguel, detalhesLink) {
   const containerInfoCarro = document.createElement("div");
   containerInfoCarro.classList.add("container-info-carro");
@@ -145,11 +144,11 @@ function criaContainerInfoCarro(valorAluguel, detalhesLink) {
   const btnAlugue = document.createElement("button");
   btnAlugue.classList.add("button-alugue");
   btnAlugue.textContent = "Alugue";
-  
+
   const spanValorAluguel = document.createElement("span");
   spanValorAluguel.classList.add("valor-aluguel");
   spanValorAluguel.textContent = valorAluguel;
-  
+
   let a = document.createElement("a");
   a.href = detalhesLink;
   a.classList.add("detalhes");
@@ -176,7 +175,6 @@ function criaContainerCarro(dados) {
 
 const carrosAluguel = document.querySelector(".carros-aluguel");
 
-
 dadosCarros.forEach((dados) => {
   const containerCarro = criaContainerCarro(dados);
   carrosAluguel.appendChild(containerCarro);
@@ -184,10 +182,160 @@ dadosCarros.forEach((dados) => {
 
 console.log("Carros adicionados:", dadosCarros.length);
 
-
 // Logica da execução da animação de flip-------------------------------------------------------------------
-
-let ancorasFlip = document.querySelectorAll('.detalhes')
+const arrayDetalhes = [
+  {
+    nome: "F-150 Shelby",
+    pessoas: 5,
+    tipoC: "flex",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "6,3 km/L",
+    estrada: "8,6 km/L",
+  },
+  {
+    nome: "Mustang Mach 1",
+    pessoas: 4,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "2 portas",
+    freio: "ABS",
+    cidade: "6,1 km/L",
+    estrada: "8,8 km/L",
+  },
+  {
+    nome: "Ford Territory Titanium",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "9,4 km/L",
+    estrada: "9,8 km/L",
+  },
+  {
+    nome: "Ranger Raptor",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "8,3 km/L",
+    estrada: "10,2 km/L",
+  },
+  {
+    nome: "Mustang GT",
+    pessoas: 4,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "6,1 km/L",
+    estrada: "8,8 km/L",
+  },
+  {
+    nome: " Ford Super Duty F-450 DRW",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "3,1 km/L",
+    estrada: "6,8 km/L",
+  },
+  {
+    nome: "Ranger XLS",
+    pessoas: 5,
+    tipoC: "disel",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "7,4 km/L",
+    estrada: "11,6 km/L",
+  },
+  {
+    nome: "Ford GT",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "5,9 km/L",
+    estrada: "8,9 km/L",
+  },
+  {
+    nome: "Mustang mach-E",
+    pessoas: 5,
+    tipoC: "eletrico",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "5,2 km/L",
+    estrada: "4,7 km/L",
+  },
+  {
+    nome: "Ford Explorer St",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "4,5 km/L",
+    estrada: "4,5 km/L",
+  },
+  {
+    nome: "Shelby GT500",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "6,4 km/L",
+    estrada: "10,2 km/L",
+  },
+  {
+    nome: "Maverick",
+    pessoas: 5,
+    tipoC: "flex",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "8,9 km/L",
+    estrada: "13,6 km/L",
+  },
+  {
+    nome: "Ford Ranger 3.2",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "4,5 km/L",
+    estrada: "4,5 km/L",
+  },
+  {
+    nome: "Ranger P703",
+    pessoas: 5,
+    tipoC: "diesel",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "8,9 km/L",
+    estrada: "10,2 km/L",
+  },
+  {
+    nome: "Bronco Sport",
+    pessoas: 5,
+    tipoC: "gasosa",
+    tipoD: "Automático",
+    portas: "4 portas",
+    freio: "ABS",
+    cidade: "8,6 km/L",
+    estrada: "10,5 km/L",
+  },
+];
+let ancorasFlip = document.querySelectorAll(".detalhes");
 
 ancorasFlip.forEach((ancora) => {
   ancora.addEventListener("click", () => {
@@ -196,142 +344,148 @@ ancorasFlip.forEach((ancora) => {
 
     if (avoAncora.classList.contains("flip-card")) {
       avoAncora.classList.remove("flip-card");
-      avoAncora.classList.add('flip-card-normal')
+      avoAncora.classList.add("flip-card-normal");
+      const newContainer = criaElementosDetalhes();
+      avoAncora.parentNode.replaceChild(avoAncora, newContainer);
       ancora.innerHTML = "detalhes";
-    } else if (avoAncora.classList.contains('flip-card-normal')) {
+    } else if (avoAncora.classList.contains("flip-card-normal")) {
       avoAncora.classList.remove("flip-card-normal");
-      avoAncora.classList.add('flip-card')
+      avoAncora.classList.add("flip-card");
+      
       ancora.innerHTML = "voltar";
-
     } else {
-      avoAncora.classList.add('flip-card')
+      avoAncora.classList.add("flip-card");
+      
+      
+      setTimeout(() => {
+        const newContainer = criaElementosDetalhes()
+        console.log(criaElementosDetalhes());
+      avoAncora.parentNode.replaceChild(newContainer, avoAncora)
+      },390)
       ancora.innerHTML = "voltar";
     }
-    
   });
 });
 
+function criaElementosDetalhes() {
+  const containerCarro = document.createElement("div");
+  containerCarro.classList.add("container-carro");
+  const containerDetalhes = document.createElement("div");
+  containerDetalhes.classList.add("container-detalhes");
 
-
-function criaElementosDetalhes(teste) {
-  const containerCarro = document.createElement('div')
-  containerCarro.classList.add('container-carro')
-  const containerDetalhes = document.createElement('div')
-  containerDetalhes.classList.add('container-detalhes')
-
-  function criaConsumo() {
-    const divConsumo = document.createElement('div')
-    divConsumo.classList.add('consumo-litragem')
-    const span1 = document.createElement('span')
-    span1.classList.add('tipo-consumo')
-    const span2 = document.createElement('span')
-    span2.classList.add("tipo-consumo");
-    const span3 = document.createElement('span')
-    span3.classList.add('litragem')
-    const span4 = document.createElement('span')
-    span4.classList.add('litragem')
-    divConsumo.append(span1, span2, span3, span4)
-   
-
-    return divConsumo
-}
+ 
   function criaLadoALado1() {
-    const ladoALado1 = document.createElement('div')
-    ladoALado1.classList.add('lado-a-lado')
-    const img1 = document.createElement('img')
+    const ladoALado1 = document.createElement("div");
+    ladoALado1.classList.add("lado-a-lado");
+    const img1 = document.createElement("img");
     img1.classList.add("img-boneco");
     img1.src = "icones/bonecos 2.svg";
-    const span1 = document.createElement('span')
-    span1.classList.add('pessoas')
-    span1.innerHTML='4 pessoas'
+    const span1 = document.createElement("span");
+    span1.classList.add("pessoas");
+    span1.innerHTML = "4 pessoas";
     ladoALado1.append(img1, span1);
     containerDetalhes.append(ladoALado1);
     return containerDetalhes;
-}
+  }
   function criaLadoALado2() {
-    const ladoALado2 = document.createElement('div')
+    const ladoALado2 = document.createElement("div");
     ladoALado2.classList.add("lado-a-lado");
-    const img2 = document.createElement('img')
-    img2.classList.add("img-boneco");
-    img2.src = "icones/bonecos 2.svg";
-    const span2 = document.createElement('span')
-    span2.classList.add('pessoas')
-    span2.innerHTML='4 pessoas'
+    const img2 = document.createElement("img");
+    img2.classList.add("img-gasosa");
+    img2.src = "icones/gasosa 5.svg";
+    const span2 = document.createElement("span");
+    span2.classList.add("gasolina");
+    span2.innerHTML = "flex";
     ladoALado2.append(img2, span2);
     containerDetalhes.append(ladoALado2);
     return containerDetalhes;
-}
+  }
   function criaLadoALado3() {
-    const ladoALado3 = document.createElement('div')
+    const ladoALado3 = document.createElement("div");
     ladoALado3.classList.add("lado-a-lado");
-    const img3 = document.createElement('img')
-    img3.classList.add("img-boneco");
-    img3.src = "icones/bonecos 2.svg";
-    const span3 = document.createElement('span')
-    span3.classList.add('pessoas')
-    span3.innerHTML='4 pessoas'
+    const img3 = document.createElement("img");
+    img3.classList.add("img-volante");
+    img3.src = "icones/volante 5.svg";
+    const span3 = document.createElement("span");
+    span3.classList.add("automatico");
+    span3.innerHTML = "automatico";
     ladoALado3.append(img3, span3);
     containerDetalhes.appendChild(ladoALado3);
     return containerDetalhes;
-}
+  }
   function criaLadoALado4() {
-    const ladoALado4 = document.createElement('div')
+    const ladoALado4 = document.createElement("div");
     ladoALado4.classList.add("lado-a-lado");
-    const img4 = document.createElement('img')
-    img4.classList.add("img-boneco");
-    img4.src = "icones/bonecos 2.svg";
-    const span4 = document.createElement('span')
-    span4.classList.add('pessoas')
-    span4.innerHTML='4 pessoas'
+    const img4 = document.createElement("img");
+    img4.classList.add("img-portas");
+    img4.src = "icones/Car Door.svg";
+    const span4 = document.createElement("span");
+    span4.classList.add("portas");
+    span4.innerHTML = "4 portas";
     ladoALado4.append(img4, span4);
     containerDetalhes.append(ladoALado4);
     return containerDetalhes;
-}
+  }
   function criaLadoALado5() {
-    const ladoALado5 = document.createElement('div')
+    const ladoALado5 = document.createElement("div");
     ladoALado5.classList.add("lado-a-lado");
-    const img5 = document.createElement('img')
-    img5.classList.add("img-boneco");
-    img5.src = "icones/bonecos 2.svg";
-    const span5 = document.createElement('span')
-    span5.classList.add('pessoas')
-    span5.innerHTML='4 pessoas'
+    const img5 = document.createElement("img");
+    img5.classList.add("img-abs");
+    img5.src = "icones/abs.svg";
+    const span5 = document.createElement("span");
+    span5.classList.add("abs");
+    span5.innerHTML = "abs";
     ladoALado5.append(img5, span5);
-    containerDetalhes.append(ladoALado5)
-    return containerDetalhes
-}
+    containerDetalhes.append(ladoALado5);
+    return containerDetalhes;
+  }
+  function criaConsumo() {
+    const divConsumo = document.createElement("div");
+    divConsumo.classList.add("consumo-litragem");
+    const span1 = document.createElement("span");
+    span1.classList.add("tipo-consumo");
+    const span2 = document.createElement("span");
+    span2.classList.add("tipo-consumo");
+    const span3 = document.createElement("span");
+    span3.classList.add("litragem");
+    const span4 = document.createElement("span");
+    span4.classList.add("litragem");
+    span1.innerHTML='teste'
+    span2.innerHTML='teste'
+    span3.innerHTML='teste'
+    span4.innerHTML='teste'
+    divConsumo.append(span1, span2, span3, span4);
 
-   
+    return divConsumo;
+  }
+  function criaA() {
+    const a = document.createElement('a')
+    a.classList.add('detalhes')
+    a.innerHTML = 'Voltar(Acho que vai dar ruim)'
+    a.href='#'
+    return a
+  }
 
-const retorno1 = criaLadoALado1()
-const retorno2 = criaLadoALado2()
-const retorno3 = criaLadoALado3()
-const retorno4 = criaLadoALado4()
-const retorno5 = criaLadoALado5()
-const retornoConsumo = criaConsumo();
-console.log(containerCarro)
-  containerCarro.appendChild(retorno1,retorno2,retorno3,retorno4,retorno5,retornoConsumo )
+  const retorno1 = criaLadoALado1();
+  const retorno2 = criaLadoALado2();
+  const retorno3 = criaLadoALado3();
+  const retorno4 = criaLadoALado4();
+  const retorno5 = criaLadoALado5();
+  const retornoConsumo = criaConsumo();
+  const retornoA = criaA();
+  // console.log(containerCarro);
+  containerCarro.append(
+    retorno1,
+    retorno2,
+    retorno3,
+    retorno4,
+    retorno5,
+    retornoConsumo,
+    retornoA
+  );
   return containerCarro;
 }
-criaElementosDetalhes()
-
-const voltarDetalhes = document.querySelectorAll('.voltar-detalhes')
-voltarDetalhes.forEach((ancora) => {
-  ancora.addEventListener("click", () => {
-    const nomeCarro = document.querySelectorAll(".nome-carro");
-    const paiAncora = ancora.parentElement;
-    const avoAncora = paiAncora.parentElement;
-    if (avoAncora.querySelector(".nome-carro")) {
-      const nomeCarro = avoAncora.querySelector(".nome-carro");
-      let naoTem;
-      if (nomeCarro) {
-        criaElementosDetalhes();
-      } else {
-        console.log("não tem");
-      }
-    }
-  });
-});
+// criaElementosDetalhes();
 
 // Lógica botão registrar -----------------------------------------------------------
 
@@ -343,17 +497,24 @@ btnRegistrar.addEventListener("click", () => {
     dataEntrega = document.querySelector(".data-entrega").value,
     horaEntrega = document.querySelector(".hora-entrega").value;
   let erro;
-  if(!inputLocal) erro = 'O local não está definido, por favor defina'
-  if(!dataRetirada) erro = "Faltam alguns campos para definir:)";
-  if(!horaRetirada) erro = "Faltam alguns campos para definir:)";
-  if(!dataEntrega) erro = "Faltam alguns campos para definir:)";
+  if (!inputLocal) erro = "O local não está definido, por favor defina";
+  if (!dataRetirada) erro = "Faltam alguns campos para definir:)";
+  if (!horaRetirada) erro = "Faltam alguns campos para definir:)";
+  if (!dataEntrega) erro = "Faltam alguns campos para definir:)";
   if (!horaEntrega) erro = "Faltam alguns campos para preencher";
 
-  if (!inputLocal&&!dataEntrega&&!dataRetirada&&!horaEntrega&&!horaRetirada) {
-    window.alert('Nenhum campo preenchido, por favor, digite as informações necessarias')
-  }
-   else if (erro) {
-   window.alert(erro)
+  if (
+    !inputLocal &&
+    !dataEntrega &&
+    !dataRetirada &&
+    !horaEntrega &&
+    !horaRetirada
+  ) {
+    window.alert(
+      "Nenhum campo preenchido, por favor, digite as informações necessarias"
+    );
+  } else if (erro) {
+    window.alert(erro);
   } else {
     const divRegistrar = document.querySelector(".div-registrar");
 
@@ -375,4 +536,3 @@ btnRegistrar.addEventListener("click", () => {
     }, 8000);
   }
 });
-
