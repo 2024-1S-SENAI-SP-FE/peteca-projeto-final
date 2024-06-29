7b1f5f5820bbd4f303952e0946992c2182462db5
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-06-29 18:18:13 -0300

    feat: adicicionando validação de caracteres no input de nome

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -1,4 +1,28 @@
-
+function bordaVermelha(index) {
+    const input = document.querySelectorAll('input')
+    const span = document.querySelector('.invalido')
+    input[index].style.border='1px solid red'
+    span[index].style.visibility='visible'
+    console.log('cor vermelha');
+}
+function bordaAzul() {
+    const input = document.querySelectorAll("input");
+    const span = document.querySelector(".invalido");
+    input[index].style.border = "1px solid red";
+    span[index].style.visibility='hidden'
+    console.log("cor azul");
+}
+function validaNome() {
+    const nome = document.querySelector('#nome')
+    if (nome.value.lenght >= 3) {
+        bordaVermelha(0)
+    } else {
+        
+    }
+}
+function invalido() {
+    const invalido = document.querySelector('.invalido')
+}
 const apiUrl = "http://localhost:3000/usuarios";
    async function cadastrar() {
     const req = {
@@ -19,7 +43,7 @@ const apiUrl = "http://localhost:3000/usuarios";
    
     fetch(newUser)
        console.log('aaaaaa');
-       
+
        window.location.href='index.html'
 }
 document.querySelector(".btn-login").addEventListener("click", cadastrar);
