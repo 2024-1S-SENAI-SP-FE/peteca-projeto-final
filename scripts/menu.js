fb1ff3d85814564630a9cfa032cad27de827e767
Rodrigo Henrique Fonseca Estrela <rohenri0808@gmail.com>
2024-07-09 19:31:59 -0300

    refactor: alterações na lógica

--- scripts/menu.js
+++ scripts/menu.js
@@ -11,18 +11,21 @@ document.addEventListener("DOMContentLoaded", function() {
     function criaAncora(url, texto) {
       const a = document.createElement("a");
 
-      a.href = url;
-      if (
-        texto ===
-        '<img src="../icones/arrow-right-to-bracket-solid copy.svg" alt="">Log in'
-      ) {
-        if (nameUser) {
-          texto = nameUser;
+        a.href = url;
+        if (
+            texto ===
+            '<img src="../icones/arrow-right-to-bracket-solid copy.svg" alt="">Log in'
+        ) {
+            if (nameUser) {
+            url = 'user.html'
+            texto = nameUser;
+            a.href = '#'
+          a.classList.add('user-logado')
         }
-        a.setAttribute("target", "_blank");
+        // a.setAttribute("target", "_blank");
       }
       a.innerHTML = texto;
-      console.log(a);
+        //   console.log(a);
       return a;
     }
 
@@ -55,6 +58,32 @@ document.addEventListener("DOMContentLoaded", function() {
     // div.innerHTML = '<img src="imagens/logo.png" alt="">';
     nav.appendChild(div);
     nav.appendChild(criaUl(links));
-    
     document.body.insertAdjacentElement('afterbegin', header);
 })
+let aClass
+setTimeout(() => {
+    
+     aClass = document.querySelector('.user-logado')
+    // console.log(aClass);
+    const liPai = aClass.parentElement
+    liPai.classList.add('liPai')
+    const divInfo = document.createElement('div')
+    divInfo.classList.add('info-user')
+    liPai.append(divInfo)
+    
+    console.log(liPai);
+    aClass.addEventListener('click',console.log('teste'))
+    
+}, 300)  
+//   function showUser(div) {
+//         div.classList.add('crescer')
+//     }
+
+
+function criaDetalhesUser(a) {
+    if (a.classList.cotains = 'user-logado') {
+        console.log('teste de classe');
+       
+        
+    }
+}
